d63b86b62fcda08956aa52c5e3803220e3951b9e
frost8111981 <73012443+frost8111981@users.noreply.github.com>
2021-07-01 08:33:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw_221_appbar/MainActivity.java
@@ -0,0 +1,21 @@
+ package com.example.hw_221_appbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+
+
+ public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+ }
